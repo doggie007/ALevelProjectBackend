c3bec8998fe9142719bda91043e52c8ab84d8c17
James Tcheng <james.tcheng1234@gmail.com>
2022-09-25 22:10:45 +0100

    Initial Commit: Created FastAPI with execute method and models for 0-dimensional EBM

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from models import EBM
+from pydantic import BaseModel
+from typing import List
+
+class ModelInput(BaseModel):
+    model_name: str # Arbitrary name for this simulation
+    initial_temperature: float # Starting temperature for this simulation
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/execute/")
+async def execute(model_input: ModelInput):
+    # Runs climate model given the model name and the initial temperature in degrees C
+    # Returns temperature and time as list of floats
+    model = EBM(model_input.model_name, model_input.initial_temperature)
+    model.run()
+    simulation_data = model.get_data()
+    return simulation_data
---
+++ models.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import climlab
+
+# Define observed real-world constants
+INSOLATION_OBSERVED = 341.3  # in W/m2
+OLR_OBSERVED = 238.5  # Outgoing longwave radiation in W/m2
+SIGMA = 5.67E-8  # Stefan-Boltzmann constant
+T_OBSERVED = 288.  # global average surface temperature
+TAU = OLR_OBSERVED / SIGMA / T_OBSERVED ** 4  # tuned value of transmissivity to model greenhouse model
+F_REFLECTED = 101.9  # reflected shortwave flux in W/m2
+ALPHA = F_REFLECTED / INSOLATION_OBSERVED # global albedo
+ASR_OBSERVED = INSOLATION_OBSERVED - F_REFLECTED  # Absorbed shortwave radiation in W/m2
+
+
+class History:
+    # Stores all the information about the output data from running the climate simulation
+    def __init__(self):
+        self.temperature = []  # in degrees Celsius
+        self.time = []  # in days
+
+    def record(self, state, current_time):
+        # Stores the data of one timestep
+        # params state: climlab domain object that stores its current information
+        # params current_time: current time elapsed from start given in seconds
+        self.temperature.extend(state.Ts[0])
+        self.time.append(current_time / (60. * 60. * 24.)) # normalise from seconds to days
+
+    def visualise(self):
+        # Plots temperature vs time graph (for debugging purposes)
+        plt.plot(self.time, self.temperature)
+        plt.xlabel("Time (days)")
+        plt.ylabel("Temperature ($^\circ$C)")
+        plt.show()
+
+
+class Simulation:
+    # Base class for the climate models to store basic information about the simulation
+    def __init__(self, name):
+        self.name = name
+        self.history = History()
+
+
+class EBM(Simulation):
+    # 0-dimensional energy balance model
+    def __init__(self, name, initial_temperature=30.0, *args, **kwargs):
+        # params name: name of simulation
+        # params initial_temperature: temperature in degrees C
+        super().__init__(name)
+        self.initial_temperature = initial_temperature
+
+    def get_data(self):
+        # Dictionary of temperature and time for ease of converting to response object
+        return {"temperatures": self.history.temperature, "times": self.history.time}
+
+    def show_history(self):
+        # Visualise temperature vs time so far
+        self.history.visualise()
+
+    def run(self):
+        # Sets up climate model and runs it in accordance with time
+        # Run time is currently fixed at 50 years / 600 months
+        # Timestep is currently fixed at 1 month
+        delta_t = 60. * 60. * 24. * 30.  # time step of 1 month
+        time_steps = 600 # 600 iterations
+
+        # Initialising components of environment and defining the interactions between
+        # Create zero-dimensional domain
+        state = climlab.surface_state(
+            num_lat=1,  # a single point
+            water_depth=100.,  # 100 meters slab of water (sets the heat capacity)
+            T0=self.initial_temperature, # global mean initial temperature
+            T2=0, # no gradient in initial temperature
+        )
+
+        # Longwave radiation process (outgoing)
+        olr = climlab.radiation.Boltzmann(name='OutgoingLongwave',
+                                          state=state,
+                                          tau=TAU,  # transmissivity of atmosphere
+                                          eps=1.,   # emissivity of surface of planet
+                                          timestep=delta_t)
+
+        # Shortwave radiation process (incoming)
+        asr = climlab.radiation.SimpleAbsorbedShortwave(name='AbsorbedShortwave',
+                                                        state=state,
+                                                        insolation=INSOLATION_OBSERVED,
+                                                        albedo=ALPHA,
+                                                        timestep=delta_t)
+
+        # Couple time-dependent processes to form EBM
+        ebm = climlab.couple([olr, asr])
+        ebm.name = self.name
+
+        # Running the simulation
+        # step forward in time to run climate model
+        self.history.record(state, 0.) # Record initial values
+        for step_num in range(1, time_steps+1):
+            ebm.step_forward()
+            self.history.record(state, step_num * delta_t)
