a83f38fa9bf1189ae6f0296d629b1e3eb6571cbc
James Tcheng <james.tcheng1234@gmail.com>
2022-10-03 10:37:48 +0100

    Added more tests for API

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from fastapi import FastAPI
 from models import ZeroDimensionalEnergyBalanceModel
 from pydantic import BaseModel
+from typing import List
 
 
 class ModelInput(BaseModel):
     model_name: str # Arbitrary name for this simulation
     initial_temperature: float # Starting temperature for this simulation
 
+class ModelOutput(BaseModel):
+    temperatures: List[float]
+    times: List[float]
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    # Debugging purposes
     return {"message": "Hello World"}
 
-
-@app.post("/execute/")
+@app.post("/execute/", response_model=ModelOutput)
 async def execute(model_input: ModelInput):
     # Runs climate model given the model name and the initial temperature in degrees C
     # Returns temperature and time as list of floats
     model = ZeroDimensionalEnergyBalanceModel(model_input.model_name, model_input.initial_temperature)
     model.run()
     simulation_data = model.get_data()
+    print(simulation_data)
     return simulation_data
--- tests/test_API.py
+++ tests/test_API.py
@@ -21,7 +21,7 @@ class TestAPIEndpoints(unittest.TestCase):
 
         response = client.post("/execute/", json={"model_name": model_name, "initial_temperature": initial_temperature})
 
-        # Run model
+        # Run model for getting the correct data
         model = ZeroDimensionalEnergyBalanceModel(model_name, initial_temperature)
         model.run()
         simulation_data = model.get_data()
@@ -29,8 +29,30 @@ class TestAPIEndpoints(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"temperatures": simulation_data["temperatures"], "times": simulation_data["times"]})
 
-    def test_execute_invalid_http_request(self):
-        pass
+    def test_execute_missing_required_parameters(self):
+        # Raises 422 error if required parameter is missing
+        # Test when parameter for initial temperature is missing
+        client = TestClient(app)
+        model_name = "Arbitrary Name"
+        response = client.post("/execute/", json={"model_name": model_name})
+        self.assertEqual(response.status_code, 422)
+
+    def test_execute_invalid_parameter_type(self):
+        # Raises 422 error if parameter input is of wrong type
+        client = TestClient(app)
+        model_name = "Arbitrary Name"
+        initial_temperature = "one-hundred"
+        response = client.post("/execute/", json={"model_name": model_name, "initial_temperature": initial_temperature})
+        self.assertEqual(response.status_code, 422)
+
+    def test_execute_invalid_parameter_value(self):
+        # Raises 400 error if initial temperature value is not within realistic range
+        client = TestClient(app)
+        model_name = "Arbitrary Name"
+        initial_temperature = 1000
+        response = client.post("/execute/", json={"model_name": model_name, "initial_temperature": initial_temperature})
+        self.assertEqual(response.status_code, 400)
+
 
 
 if __name__ == '__main__':
